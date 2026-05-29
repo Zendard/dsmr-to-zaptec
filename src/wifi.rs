a3b03bde8f3779356ec6175225da922b496c513b
Zendard <thijsmuylaert@hotmail.com>
2026-05-29 23:48:31 +0200

    Make wifi work

--- src/wifi.rs
+++ src/wifi.rs
@@ -18,7 +18,7 @@ use reqwless::{
 
 const SSID: &str = env!("WIFI_SSID");
 const PASSWORD: &str = env!("WIFI_PASSWORD");
-const CERT_BYTES: &[u8] = include_bytes!(env!("CERT_FILE"));
+const CERT_BYTES: &[u8] = concat!(include_str!(env!("CERT_FILE")), "\0").as_bytes();
 
 pub async fn init_wifi(
     wifi: WIFI<'static>,
@@ -60,6 +60,7 @@ async fn connection(mut controller: WifiController<'static>) {
 
     loop {
         if matches!(controller.is_connected(), Ok(true)) {
+            info!("Connected to wifi");
             controller.wait_for_event(WifiEvent::StaDisconnected).await;
             Timer::after_secs(5).await;
         }
@@ -68,6 +69,7 @@ async fn connection(mut controller: WifiController<'static>) {
             start_controller(&mut controller).await;
             scan_wifi(&mut controller).await;
 
+            info!("Connecting");
             match controller.connect_async().await {
                 Ok(_) => info!("Wifi connected"),
                 Err(e) => {
@@ -86,6 +88,7 @@ async fn start_controller(controller: &mut WifiController<'static>) {
             .with_ssid(SSID.into())
             .with_password(PASSWORD.into()),
     );
+    info!("{:?}", &config);
     controller.set_config(&config).unwrap();
     info!("Starting wifi...");
     controller.start_async().await.unwrap();
@@ -94,10 +97,10 @@ async fn start_controller(controller: &mut WifiController<'static>) {
 
 async fn scan_wifi(controller: &mut WifiController<'static>) {
     info!("Scanning wifi networks...");
-    let config = ScanConfig::default().with_ssid(SSID).with_max(10);
+    let config = ScanConfig::default().with_max(10);
     let result = controller.scan_with_config_async(config).await.unwrap();
     info!("Scanned wifi networks");
-    info!("{:?}", result[0]);
+    info!("{:?}", result.as_slice());
 }
 
 #[embassy_executor::task]
@@ -110,13 +113,14 @@ pub type ReqwlessClient =
 pub type ReqwlessConnection<'a> = TcpConnection<'a, 1, 4096, 4096>;
 
 async fn make_reqwless_client(stack: Stack<'static>) -> ReqwlessClient {
+    info!("Building Reqwless client");
     let state = mk_static!(TcpClientState<1,4096,4096>, TcpClientState::<1, 4096, 4096>::new());
     let tcp_client = mk_static!(TcpClient<1,4096,4096>,TcpClient::new(stack, state));
     let trng = mk_static!(Trng, Trng::try_new().unwrap());
     let mbedtls_instance = mk_static!(mbedtls_rs::Tls, mbedtls_rs::Tls::new(trng).unwrap());
     let dns_socket = mk_static!(DnsSocket, DnsSocket::new(stack));
 
-    let cstr_cert: &CStr = CStr::from_bytes_until_nul(CERT_BYTES).unwrap();
+    let cstr_cert: &CStr = CStr::from_bytes_with_nul(CERT_BYTES).unwrap();
 
     let tls_config = TlsConfig::new(
         TlsVersion::Tls1_3,
--- tests/hello_test.rs
+++ tests/hello_test.rs
@@ -4,24 +4,83 @@
 
 #![no_std]
 #![no_main]
-#[cfg(test)]
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[cfg(test)]
 #[embedded_test::tests(executor = esp_rtos::embassy::Executor::new())]
 mod tests {
-    use defmt::assert_eq;
+    use core::error::Error;
+    use defmt::info;
+    use dsmr_to_zaptec::*;
+    use embassy_net::StackResources;
+    use embedded_io::Error as EmbeddedError;
+    use esp_hal::{clock::CpuClock, peripherals::WIFI, rng::TrngSource, timer::timg::TimerGroup};
+
+    macro_rules! mk_static {
+        ($t:ty) => {{
+            static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+            STATIC_CELL.uninit()
+        }};
+        ($t:ty,$val:expr) => {{ mk_static!($t).write($val) }};
+    }
+
+    struct Init<'a> {
+        wifi: WIFI<'a>,
+        spawner: embassy_executor::Spawner,
+        stack_resources: &'a mut StackResources<3>,
+    }
 
     #[init]
-    fn init() {
-        let peripherals = esp_hal::init(esp_hal::Config::default());
-
-        let timg1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
-        let sw_interrupt =
-            esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-        esp_rtos::start(timg1.timer0, sw_interrupt.software_interrupt0);
+    async fn init() -> Init<'static> {
+        info!("Starting...");
 
         rtt_target::rtt_init_defmt!();
+
+        let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+        let peripherals = esp_hal::init(config);
+
+        esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
+
+        let timg0 = TimerGroup::new(peripherals.TIMG0);
+        let sw_interrupt =
+            esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+        esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+        let _trng_src = TrngSource::new(peripherals.RNG, peripherals.ADC1);
+
+        let stack_resources = mk_static!(StackResources<3>, StackResources::new());
+
+        let spawner = unsafe { embassy_executor::Spawner::for_current_executor() }.await;
+        info!("Embassy initialized!");
+
+        Init {
+            wifi: peripherals.WIFI,
+            spawner,
+            stack_resources,
+        }
+    }
+
+    #[test]
+    async fn init_wifi(init: Init<'static>) {
+        wifi::init_wifi(init.wifi, init.stack_resources, init.spawner).await;
+    }
+
+    #[test]
+    async fn http_request(init: Init<'static>) {
+        let mut https_client = wifi::init_wifi(init.wifi, init.stack_resources, init.spawner).await;
+        let mut rx_buf = [0u8; 4096];
+
+        let request = https_client
+            .request(reqwless::request::Method::GET, "http://example.com/")
+            .await;
+
+        if let Err(e) = &request {
+            info!("Error kind: {:?}", e.kind());
+        }
+        let mut request = request.unwrap();
+
+        let response = request.send(&mut rx_buf).await.unwrap();
+
+        assert!(response.status.is_successful());
     }
 }
