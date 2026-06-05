ddf70b2e896eb29a1db6d03dcc1e7dbdd96d73f9
Zendard <thijsmuylaert@hotmail.com>
2026-06-05 20:31:25 +0200

    Add wifi errors for result types

--- src/error/wifi.rs
+++ src/error/wifi.rs
@@ -1,11 +1,15 @@
 use esp_hal::rng::TrngError;
 use esp_radio::InitializationError;
+use mbedtls_rs::{SessionError, TlsError, sys::MbedtlsError};
 
 #[derive(Debug, defmt::Format)]
 pub enum WifiError {
     ControllerInit(InitializationError),
     Wifi(esp_radio::wifi::WifiError),
     Trng(TrngError),
+    Tls(TlsError),
+    CertificateReading(SessionError),
+    MbedTls(MbedtlsError),
 }
 impl From<InitializationError> for WifiError {
     fn from(e: InitializationError) -> Self {
@@ -22,5 +26,23 @@ impl From<esp_radio::wifi::WifiError> for WifiError {
 impl From<TrngError> for WifiError {
     fn from(e: TrngError) -> Self {
         Self::Trng(e)
+    }
+}
+
+impl From<TlsError> for WifiError {
+    fn from(e: TlsError) -> Self {
+        Self::Tls(e)
+    }
+}
+
+impl From<MbedtlsError> for WifiError {
+    fn from(e: MbedtlsError) -> Self {
+        Self::MbedTls(e)
+    }
+}
+
+impl From<SessionError> for WifiError {
+    fn from(e: SessionError) -> Self {
+        Self::CertificateReading(e)
     }
 }
